0687757a8c7003d9cd21f52e8f20a9d0b83bc979
gellel <lindsay.gelle@gmail.com>
2017-02-03 18:23:12 +1100

    Testing comment structure.

--- js/lib/prototypes.js
+++ js/lib/prototypes.js
@@ -1,103 +1,201 @@
-/*
- *  Extension script for browser extension.
- *  Manages required shorthand built-in functions.
- *  File appended to event page, popup page.
-*/
-
-
 Array.prototype.append = function () {
-    // Iterate arguments sequence.
-    // Push argument at index to instance.
-    for (let i = 0; i < arguments.length; i++) this.push(arguments[i]);
+    /**
+    *** Array.append(@args);
+    *
+    * Modifies array instance. 
+    * Pushes arguments to array.
+    * Accepts argument sequence.
+    * Returns @array.
+    *
+    **/
 
-    // @return: @type: <array>
+    // Process arguments sequence. Append contents to array.
+    for (let i = 0; i < arguments.length; i++) this.push(arguments[i])
+    
     return this;
 };
 
 
 Array.prototype.empty = function () {
-    // Subset arguments from instance.
-    // Sets instance to empty.
-    this.splice(0, this.length);
+    /**
+    *** Array.append(@void);
+    *
+    * Modifies instance. 
+    * Empties array contents.
+    * Accepts void.
+    * Returns @array.
+    *
+    **/
+
+    // Subset array instance.
+    this.slice(0, this.length);
     
-    // @return: @type: <array>
     return this;
 };
 
 
 Array.prototype.set = function () {
-    // @return: @type: <array>
+    /**
+    *** Array.set(@void);
+    *
+    * Modifies instance. 
+    * Empties duplicate properties.
+    * Accepts void.
+    * Returns @array.
+    *
+    **/
+    
     return Array.from(new Set(this));
 };
 
 
+Object.prototype.stringify = function () {
+    /**
+    *** Object.stringify(@void)
+    *
+    * Modifies instance.
+    * Updates object to string type.
+    * Accepts void.
+    * Returns @string.
+    *
+    **/
+
+    return JSON.stringify(this);
+};
+
+
+Object.prototype.editKeys = function () {
+    /**
+    *** Object.editKeys(@args)
+    *
+    * Modifies instance.
+    * Edits keys for object.
+    * Accepts argument sequence.
+    * Returns @object.
+    *
+    **/
+
+    // Process arguments sequence. 
+    // Process keys for argument at index. 
+    // Edit property for defined and unique key.
+    for (let i = 0, a = Array.prototype.slice.call(arguments).filter(function (i) { return i instanceof Object ? i : !1; }); i < a.length; i++) 
+        for (key in a[i]) if (key in this && this.hasOwnProperty(key)) this[key] = a[i][key]; 
+
+    return this;
+};
+
+
+String.prototype.object = function () {
+    /**
+    *** String.object(@void)
+    *
+    * Modifies instance.
+    * Updates string to object type.
+    * Accepts void.
+    * Returns @object.
+    *
+    **/
+
+    // Set regular expression pattern. 
+    // Matches simple object structures.
+    let regexp = /^\{((\s?)*(\"[^"]+\"(\s?)*:(\s?)*(\d|\"[^"]*\"|\{.*\}|\[.*\])+,?)+(\s?))*\}$/gi;
+
+    return regexp.test(this) ? JSON.parse(this) : {};
+};
+
+
 Element.prototype.insertNode = function () {
-    // Set arguments to array.
+    /**
+    *** Element.insertNode(@args)
+    *
+    * Creates the HTML element specified by tagName.
+    * Accepts argument sequence.
+    * Returns @element.
+    * 
+    **/
+
     let a = Array.prototype.slice.call(arguments);
 
-    // Shift first item from arguments.
-    // Set argument to string. 
-    // Construct HTMLNode from string.
+    // Create and append HTML element. Use first argument as element type.
     let e = this.appendChild(document.createElement(a.shift().toString()));
-
-    // Iterate over arguments sequence. 
-    // Set strings to TextNodes.
-    // Set objects to attributes.
-    for (let i = 0; i < a.length; i++) a[i] instanceof Object ? e.setAttributes(a[i]) : typeof a[i] === "string" && e.insertTextNode(a[i]);
     
-    // @return: @type: <HTMLNode>
+    // Process arguments sequence. 
+    // Set strings to textNodes. Set objects to HTML attributes.
+    for (let i = 0; i < a.length; i++) 
+        a[i] instanceof Object ? 
+            e.setAttributes(a[i]) : typeof a[i] === "string" && e.insertTextNode(a[i]);
+    
     return a.slice(-1)[0] instanceof Function ? a.slice(-1)[0](e, a) : e;
 };
 
-Element.prototype.insertTextNode = function () {
-    // Set arguments to array.
-    let a = Array.prototype.slice.call(arguments);
 
-    // Iterate over arguments sequence.
-    // Set argument to string.
-    // Construct TextNode from strings.
-    for (let i = 0; i < arguments.length; i++) this.appendChild(document.createTextNode(arguments[i].toString()));
+Element.prototype.insertTextNode = function () {
+    /**
+    *** Element.insertTextNode(@args)
+    *
+    * Creates the HTML text nodes for element.
+    * Accepts argument sequence.
+    * Returns @element.
+    * 
+    **/
+
+    // Process arguments sequence. 
+    // Filter array to contain strings or numbers. Set text node from argument at index.
+    for (let i = 0, a = Array.prototype.slice.call(arguments).filter(function (i) { return typeof i === "string" || typeof i === "number" ? i : !1; }); i < a.length; i++) 
+        this.appendChild(document.createTextNode(a[i]));
     
-    // @return: @type: <HTMLNode>
     return this;
 };
 
 
 Element.prototype.setAttributes = function (attributes) {
-    // Set argument to object structure.
-    // Iterate over keys in object.
-    // Set attribute from key. 
-    // Use value from object key pair.
-    for (let key in (attributes = attributes instanceof Object ? attributes : {})) this.setAttribute(key, attributes[key]);
+    /**
+    *** Element.setAttributes(@object)
+    *
+    * Sets attributes for element.
+    * Accepts object type.
+    * Returns @element.
+    * 
+    **/
+
+    // Process keys in object argument.
+    // Set HTML attribute for current key at index.
+    for (let key in (attributes = attributes instanceof Object ? attributes : {})) 
+        this.setAttribute(key, attributes[key]);
     
-    // @return: @type: @HTMLNode
     return this;
 };
 
 
 Element.prototype.remove = function () {
-    // Navigate to parentElement for HTMLNode.
-    // Remove reference HTMLNode from Document Object Module.
+    /**
+    *** Element.remove(@void)
+    *
+    * Removes element from document tree.
+    * Accepts void.
+    * Returns: @void.
+    *
+    **/
+
     this.parentElement.removeChild(this);
 };
 
 
 NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
-    // Iterate over childNodes for HTMLNode.
-    // Test childNode at index and parentNode are defined.
-    // Remove reference HTMLNode from Document Object Module.
-    for (let i = 0, len = this.length; i < len; i++) if (this[i] && this[i].parentElement) this[i].parentElement.removeChild(this[i]);
+    /**
+    *** Element.remove(@void)
+    *
+    * Removes childNodes from document tree.
+    * Accepts void.
+    * Returns: @void.
+    *
+    **/
+
+    // Process child nodes for element.
+    // Remove element from document tree.
+    for (let i = 0, len = this.length; i < len; i++) 
+        if (this[i] && this[i].parentElement) 
+            this[i].parentElement.removeChild(this[i]);
 };
 
-
-Object.prototype.stringify = function () {
-    // @return: @type: <string>
-    return JSON.stringify(this);
-};
-
-
-String.prototype.objectify = function () {
-    // @return: @type: <object>
-    try { return JSON.parse(this); } catch(e) { return {}; }
-};
 
