4135362805a22f0aa758511997993b07f0506d32
gellel <lindsay.gelle@gmail.com>
2017-02-15 10:58:42 +1100

    Updating component method to new API.

--- js/lib/component.js
+++ js/lib/component.js
@@ -1,24 +1,15 @@
-class Component {
+class Component extends HTMLElement {
 
+	static Get (name) {
+
+		return window.customElements.get(String(name));
+	}
+	
 	static Name () {
-		/**
-		*** Formats custom HTML tag names.
-		*
-		* Accepts multiple string arguments.
-		* Edits arguments to match string pattern for custom HTML.
-		* Strings are separated by hyphens and uppercase characters.
-		*
-		**/
-
-		/**
-	    *** @param: arguments, @type: sequence.
-	    *
-	    *** @return: @type: string.
-	    */
 
 		let a = Array.prototype.slice.call(arguments);
 
-		a = a.filter(function (i) { return typeof i === 'string' ? i : !1 });
+		a = a.filter(function (i) { if (typeof i === 'string') return i; });
 
 		a = a.map(function (i) { return i.replace(/[^a-zA-Z\-]/gi, '').split(/\-|(?=[A-Z])/g); });
 
@@ -29,59 +20,27 @@ class Component {
 		return (a).join('-').toLowerCase();
 	}
 
-	static Config (config) {
-		/**
-		*** Forats custom HTML config objects.
-		*
-		* Accepts object argument.
-		* Assigns primary prototype keys.
-		* Key names are kept in object.
-		*
-		**/
-
-		/**
-	    *** @param: config, @type: object.
-	    *
-	    *** @return: @type: object.
-	    */
-
-		config = config instanceof Object ? config : new Object();
-
-		for (let i = 0, k = ['prototype', 'extends']; i < k.length; i++)
-			if (!config.hasOwnProperty(k[i])) 
-				config[k[i]] = undefined;
-
-		return config;
-	}
 
 	static Register () {
-		/** 
-		*** Sets custom HTML in browser config.
-		*
-		* Accepts multiple string and object arguments.
-		* Strings are cast as parameters for Component.Name.
-		* Objects are cast as parameters for Component.Config.
-		*
-		**/
-
-		/**
-	    *** @param: config, @type: object.
-	    *
-	    *** @return: @type: HTMLElement.prototype.
-	    */
 
 		let a = Array.prototype.slice.call(arguments);
 
-		let o = new Object();
+		let name = Component.Name.apply(null, a.filter(function (i) { if (typeof i === 'string') return i; }));
 
-		let n = a.filter(function (i) { if (typeof i === 'string') return i; });
+		let config = a.reduce(function(result, current) {
+			for (let key in current)
+				if (current.hasOwnProperty(key)) 
+					result[key] = current[key];
+			
+			return result;
+		}, {});
 
-		let c = a.filter(function (i) { if (i instanceof Object) return i; });
+		let constructor = a.slice(-1)[0];
 
-		for (let i = 0; i < c.length; i++) Object.assign(o, c[i])
+		constructor = constructor.prototype instanceof HTMLElement ? constructor : class extends HTMLElement { constructor (config) { super(); for (let key in (config = config instanceof Object ? config : {})) this[key] = config[key]; }};
 
-		let e = document.registerElement(Component.Name.apply(null, n), Component.Config(o));
+		if (!Component.Get(name)) window.customElements.define(name, constructor);
 
-		return new e();
+		return document.createElement(name);
 	}
 }
