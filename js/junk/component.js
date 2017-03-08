355261f7e7e2213d20374edca846cbd30dd9795b
gellel <lindsay.gelle@gmail.com>
2017-03-08 18:44:24 +1100

    Editing constructor tests.

---
+++ js/junk/component.js
@@ -0,0 +1,125 @@
+class HTMLComponent extends HTMLElement {
+	
+	get componentAppBase () {
+		let e = document.createElement('div');
+		e.setAttribute('data-component-base', '');
+		e.setAttribute('data-constructor-name', this.constructor.name);
+		e.setAttribute('data-constructor-prototype', Object.getPrototypeOf(this.constructor).name);
+
+		return e;
+	}
+
+	get componentAppSections () {
+		for (var i = 0, o = {}, s = this.querySelectorAll('[data-component-section'), l = s.length; i < l; i++)
+			if (s[i].hasAttribute('data-component-id'))
+				Object.assign(o, {[s[i].getAttribute('data-component-id')]:s[i]});
+
+		return o;
+	}
+
+	get componentAppProperties () {
+		return this.__base__.properties;
+	}
+
+	get componentAppStates () {
+		return this.__base__.states;
+	}
+
+	get componentAppState () {
+		for (let key in this.__base__.states)
+			if (this.__base__.states.hasOwnProperty(key))
+				if (this.__base__.states[key])
+					return key;
+
+		return null;
+	}
+
+	set componentAppState (state) {
+		for (let key in this.__base__.states)
+			if (this.__base__.states.hasOwnProperty(key))
+				if (!(key === state)) 
+					this.__base__.states[key] = false;
+
+		if (typeof state === 'string') 
+			this.__base__.states[state] = true;
+
+		this.componentAppState ? 
+			this.setAttribute('data-component-state', this.componentAppState) : 
+			this.removeAttribute('data-component-state');
+
+		return state;
+	}
+
+	addComponentAppSection (parent, element, attributes) {
+		if (this === parent) return;
+
+		if (parent instanceof Element && this.contains(parent))
+	}
+
+	addComponentAppState () {
+		if (arguments.length)
+			for (let i = 0, l = arguments.length; i < l; i++)
+				this.__base__.states[arguments[i]] = false;
+
+		return this;
+	}
+
+	removeComponentAppState () {
+		if (arguments.length)
+			for (let i = 0, l = arguments.length; i < l; i++)
+				delete this.__base__.states[arguments[i]];
+
+		return this;
+	}
+ 
+	addComponentAppProperties (properties) {
+		if (properties instanceof Object && Object.keys(properties).length)
+			for (let key in properties)
+				if (properties.hasOwnProperty(key))
+					this.__base__.properties[key] = properties[key];
+
+		return this;
+	}
+
+	removeComponentAppProperties () {
+		if (arguments.length)
+			for (let i = 0, l = arguments.length; i < l; i++)
+				delete this.__base__.properties[arguments[i]];
+
+		return this;
+	}
+
+	connectedCallback () {
+		this.appendChild(this.componentAppBase);
+
+		if (this.onConnect && this.onConnect instanceof Function)
+			this.onConnect();
+	}
+
+	disconnectedCallback () {
+		if (this.onDisconnect && this.onDisconnect instanceof Function)
+			this.onDisconnect();
+	}
+
+	constructor () {
+		super();
+
+		Object.assign(this, {__base__:{
+			properties:{}, states:{}}});
+
+		if (this.onConstruct && this.onConstruct instanceof Function)
+			this.onConstruct();
+	}
+}
+
+
+
+
+
+customElements.define('component-element', HTMLComponent);
+
+var i = document.createElement('component-element');
+
+document.body.appendChild(i);
+
+clear();
--- js/junk/temp.js
+++ js/junk/temp.js
@@ -1,157 +1,129 @@
-class Copy extends HTMLElement {
+class Component extends HTMLElement {
 
-    ['assign-text-nodes'] () {
-        /* element, [strings,..], new String() */
-        let a = Array.prototype.slice.call(arguments);
-
-        let e = a.shift();
-
-        let j = a.slice(-1)[0] instanceof String ? a.slice(-1)[0].valueOf() : false;
-
-        if (e instanceof Element) {
-
-            let c = e.firstChild;
-            let n;
-
-            while (c) {
-                n = c.nextSibling;
-                if (c.nodeType === 3) {
-                   e.removeChild(c);
-                } 
-                c = n;
-            }
-
-            a = a.filter(function (i) { 
-                if (typeof i === 'string') return i });
-
-            for (let i = 0, l = a.length; i < l; i++) {
-                
-                e.appendChild(document.createTextNode(a[i]))
-
-                if (j) e.appendChild(document.createTextNode(j))
-            }
-        }
-
-        return this;
-    }
-
-    ['assign-component-arguments'] (e, parameters) {
-        if (this['has-decendant'](e))
-            if (e.hasAttribute('data-component') && parameters instanceof Object)
-                e.setAttribute('data-component-arguments', JSON.stringify(parameters))
-
-        return this;
-    }
-
-    ['assign-component-method'] (e, method) {
-        if (this['has-decendant'](e)) 
-            if (e.hasAttribute('data-component') && typeof method === 'string')
-                if (this[method] && this[method] instanceof Function)
-                    e.setAttribute('data-component-method', method)
-
-        return this;
-    }
-
-    ['assign-component-id'] (e, id) {
-        if (this['has-decendant'](e)) 
-            if (e.hasAttribute('data-component') && typeof id === 'string')
-                e.setAttribute('data-component-id', id)
-
-        return this;
-    }
-
-    ['has-decendant'] (e) {
-        return e.__proto__ instanceof Element && this.contains(e) ? true : false;
-    }
-
-    ['has-assertion'] (e) {
-       return (this['is-component'](e) && e.hasAttribute('data-component-method') ? true : false;
-    }
     
-    ['is-component'] (e) {
-        return (this['has-decendant'](e) && e.hasAttribute('data-component')) ? true : false;
-    }
-
-
-    get ['get-component-structure'] () {
-        let e = document.createElement('div');
-
-        e.setAttribute('data-component', '');
-        e.setAttribute('data-component-id', 'title');
-        e.setAttribute('data-component-method','assign-text-nodes');
-        e.setAttribute('data-component-arguments','hello world');
-
-        return e;
-    }
-
-    get ['get-component-sections'] () {
-        let s = this.querySelectorAll('[data-component]');
-
-        for (var i = 0, o = {}, l = s.length; i < l; i++)
+    get ['component-properties'] () {
+        for (var i = 0, o = {}, s = this.querySelectorAll('[data-component-property]'), l = s.length; i < l; i++)
             if (s[i].hasAttribute('data-component-id'))
-                Object.assign(o, {[s[i].getAttribute('data-component-id')]:s[i]})
+                Object.assign(o, {[s[i].getAttribute('data-component-id')]:s[i]});
 
         return o;
     }
 
+    ['propegate-method'] (e, prop) {
+
+        let a = e.getAttribute('data-component-method');
+
+        if (e.__proto__ instanceof Element && this[a] instanceof Function)
+            this[a](e, prop, (e.hasAttribute('data-method-arguments') ? 
+                JSON.parse(e.getAttribute('data-method-arguments')) : {}));
+
+        return e;
+    }
+
     ['propegate-properties'] (properties) {
-        let s = this['get-component-sections'];
+        let s = this['component-properties'];
 
         for (let key in s)
             if (s.hasOwnProperty(key))
-                if (this['has-assertion'](s[key]))
-                    console.log(key, s[key]);
-
-        console.log('%csections', 'color:red;font-weight:bold;', s);
+                if (s[key].hasAttribute('data-component-property') && s[key].hasAttribute('data-component-id'))
+                    if (properties[s[key].getAttribute('data-component-id')])
+                        this['propegate-method'](s[key], properties[s[key].getAttribute('data-component-id')])
 
         return this;
     }
 
-    set assertProperties (properties) {
+    assertMethods (methods) {
+        if (methods instanceof Object && Object.keys(methods).length)
+            for (let key in methods)
+                if (methods.hasOwnProperty(key))
+                    if (!this.__proto__[key] && methods[key] instanceof Function)
+                        this[key] = methods[key];
+        
+        return this;
+    }
+
+    assertProperties (properties) {
         if (properties instanceof Object && Object.keys(properties).length)
             for (let key in properties)
                 if (properties.hasOwnProperty(key))
                     this.properties[key] = properties[key];
 
-        return this.properties;
+        return this;
     }
 
     propegateProperties (properties) {
-        if (properties instanceof Object && Object.keys(properties).length)
-            for (let key in properties)
-                if (properties.hasOwnProperty(key))
-                    this.properties[key] = properties[key];
+        
+        this.assertProperties(properties);
         
         this['propegate-properties'](this.properties);
 
         return this;
     }
 
-    disconnectedCallback () {
-        console.log('%cCopyCat %cflees!', 'color:#bbcccb;font-style:italic;', 'color:#bbcccb;font-weight:bold;');
-    }
-
-    connectedCallback () {
-        /* set inner */
-        this.appendChild(this['get-component-structure']);
-        /* set properties for sections */
-        this['propegate-properties'](this.properties);
-
-        console.log('%cA new CopyCat %cpounces %conto the page!', 'color:#bbcccb;font-style:italic;', 'color:#bbcccb;font-weight:bold;', 'color:#bbcccb;font-style:italic;');
-    }
-
     constructor () {
+        
         super(arguments);
 
         Object.assign(this, {properties: new Object()});
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+class Copy extends Component {
+
+    get ['component-architecture'] () {
+        let e = document.createElement('div');
+        e.setAttribute('data-component-base', '');
+        
+        let s = document.createElement('div');
+        s.setAttribute('data-component-property', '');
+        s.setAttribute('data-component-id', 'title');
+        s.setAttribute('data-component-method', 'append-text-nodes');
+      
+        e.appendChild(s);
+
+        return e;
+    }
+
+
+
+    disconnectedCallback () {
+        console.log('%cCopyCat %cflees!', 'color:#bbcccb;font-style:italic;', 'color:#bbcccb;font-weight:bold;');
+    }
+
+    connectedCallback () {
+
+        this.appendChild(this['component-architecture']);
+
+        this['propegate-properties'](this.properties);
+
+        console.log('%cA new CopyCat %cpounces %conto the page!', 'color:#bbcccb;font-style:italic;', 'color:#bbcccb;font-weight:bold;', 'color:#bbcccb;font-style:italic;');
+
+        delete this.__proto__['component-architecture'], this.__proto__.connectedCallback;
+    }
+
+    constructor () {
+        super();
+    }
+}
+
+
 customElements.define('copycat-copy', Copy);
 
 var i = document.createElement('copycat-copy');
 
-if(window.clear) clear();
+i.assertProperties({'title':'hello world'})
+
+if (window.clear) clear();
 
 document.body.appendChild(i);
 
